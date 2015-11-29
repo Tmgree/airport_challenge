6b163f512d3fc3609839ac462c60b9b8958231e2
tmgree <tmgree@outlook.com>
2015-11-29 20:35:06 +0000

    ddddd

--- lib/airport.rb
+++ lib/airport.rb
@@ -2,17 +2,17 @@ require_relative 'plane'
 
 class Airport
   def initialize
-    
+    @planes= []
   end
   attr_reader :planes
   def land(plane)
-    fail 'Airport is full' if @plane
-@plane=plane
+    fail 'Airport is full' if @planes.count >= 20
+@planes << plane
   end
 
   def release_plane
-    fail 'No planes landed' unless @plane
-@plane
+    fail 'No planes landed' if @planes.empty?
+@planes.pop
   end
 
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -19,7 +19,7 @@ end
   end
   describe '#land' do
     it 'raises an error when full' do
-      20.times {subject.land(Plane.new)}
+      20.times {subject.land Plane.new}
       expect { subject.land Plane.new }.to raise_error 'Airport is full'
     end
   end

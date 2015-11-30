2e63761089192ee3169acb991a9cb8946c0d1509
tmgree <tmgree@outlook.com>
2015-11-30 12:18:37 +0000

    hh

--- lib/airport.rb
+++ lib/airport.rb
@@ -9,19 +9,20 @@ class Airport
   attr_reader :planes
   def land(plane)
     fail 'Airport is full' if full?
-@planes << plane
+    @planes << plane
   end
 
   def release_plane
     fail 'No planes landed' if empty?
-@planes.pop
+    @planes.pop
   end
+  
   def full?
     planes.count >= DEFAULT_CAPACITY
   end
 
   def empty?
-  @planes.size == 0 ? true : false
+    @planes.size == 0 ? true : false
 
   end
 
